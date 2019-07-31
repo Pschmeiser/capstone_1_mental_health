9a3d56ee0e2f4f58aa285a5f5355134c8f7d747c
Peter Schmeiser <Drumai@Peters-MacBook-Pro.local>
2019-07-31 10:47:23 -0600

    updated model files

--- gradient_boosted.py
+++ gradient_boosted.py
@@ -17,21 +17,40 @@ import pandas as pd
 from sklearn import metrics
 from sklearn.inspection.partial_dependence import partial_dependence, plot_partial_dependence
 
+#Load Model
+GB =GradientBoostingClassifier(n_estimators=100, max_features=24)
 
-GB =GradientBoostingClassifier()
+#Load Data
 X_data = pd.read_csv('data/cleaned_data.csv', index_col= 0)
 y_data = pd.read_csv('data/target_data.csv', index_col= 0)
+
+#Train Test Split
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data['treatment'], test_size=0.33, random_state=42)
+
+#Fit Model
 GB = GB.fit(X_train, y_train)
 best_rf_model = open('models/best_GB_model2.pkl', 'wb')
 pickle.dump(GB, best_GB_model)
+
+#Load Features
 features = GB.feature_importances_
 feature_names = X_data.columns
 df_importances = pd.DataFrame(feature_names)
 df_importances['features'] = features
 df_importances.sort_values('features',ascending=False,inplace=True)
+
 #Feature importance barplot
 df_importances.head()
 plt.figure(figsize= (12,8))
 sns.barplot(df_importances['features'][:10], feature_names[:10])
 plt.tight_layout()
+
+#Metrics
+y_pred=rf.predict(X_test)
+accuracy = metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+precision = metrics.precision_score(y_true=y_test, y_pred=y_pred)
+recall = metrics.recall_score(y_true=y_test, y_pred=y_pred)
+
+print('accuracy: {:.3f}'.format(accuracy))
+print('precision: {:.3f}'.format(precision))
+print('recall: {:.3f}'.format(recall))
--- randomforest.py
+++ randomforest.py
@@ -18,23 +18,39 @@ from sklearn import metrics
 
 
 rf = RandomForestClassifier(n_estimators=50, max_depth=50, n_jobs= -1, random_state=0, class_weight= 'balanced')
+#Load Data
 X_data = pd.read_csv('data/cleaned_data.csv', index_col= 0)
 y_data = pd.read_csv('data/target_data.csv', index_col= 0)
+
+#Train test and split
 X_train, X_test, y_train, y_test = train_test_split(X_data, y_data['treatment'], test_size=0.33, random_state=42)
+
+#Fit the model
 rf = rf.fit(X_train, y_train)
+
+#Open pickle and save model
 best_rf_model = open('models/best_rf_model.pkl', 'wb')
 pickle.dump(rf, best_rf_model)
 
+#Pull out features
 features = rf.feature_importances_
 feature_names = X_data.columns
 df_importances = pd.DataFrame(feature_names)
-
 df_importances['features'] = features
 df_importances.sort_values('features',ascending=False,inplace=True)
-df_importances.head()
 
 #Feature importance barplot
-df_importances.head()
 plt.figure(figsize= (12,8))
 sns.barplot(df_importances['features'][:10], feature_names[:10])
 plt.tight_layout()
+
+#Metrics
+y_pred=rf.predict(X_test)
+accuracy = metrics.accuracy_score(y_true=y_test, y_pred=y_pred)
+precision = metrics.precision_score(y_true=y_test, y_pred=y_pred)
+recall = metrics.recall_score(y_true=y_test, y_pred=y_pred)
+
+
+print('accuracy: {:.3f}'.format(accuracy))
+print('precision: {:.3f}'.format(precision))
+print('recall: {:.3f}'.format(recall))
